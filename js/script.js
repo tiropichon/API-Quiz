cba83c7a80708dd1f7e97d215ed97a2db53ca46a
Mario Ruiz <82596685+tiropichon@users.noreply.github.com>
2023-12-15 23:49:07 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
-const url="https://opentdb.com/api.php?amount=10";
+
 
 //fetch(url)
   //.then(res => res.json())
   //.then(json => console.log(json.results))
   //.catch(error => console.error(error))
 
-async function obtenerDatos(){
+	//for(let i=0; i<datos.results.length; i++){
+		//console.log(datos.results[i]);
+		//console.log('Pregunta: ' + datos.results[i].question + '\n' +
+		//'Respuesta correcta: '+ datos.results[i].correct_answer+'\n' +
+		//'Respuestas incorrectas: ' + datos.results[i].incorrect_answers + '\n'
+		//)
+	//}
+
+async function obtenerDatos(url){
 	const resultado = await fetch(url);
 	const datos = await resultado.json();
 	for(let i=0; i<datos.results.length; i++){
-		console.log(datos.results[i]);
-		console.log('Categoría: ' + datos.results[i].category+'\n' +
-		'Respuesta correcta: '+ datos.results[i].correct_answer+'\n' +
-		'Dificultad: '+ datos.results[i].difficulty+'\n' +
-		'Respuestas incorrectas: ' + datos.results[i].incorrect_answers + '\n' +
-		'Pregunta: ' + datos.results[i].question + '\n' +
-		'Tipo de pregunta: ' + datos.results[i].type + '\n')
+		console.log(datos.results[i].question + '\n' +
+								datos.results[i].correct_answer+ '\n');
+		datos.results[i].incorrect_answers.forEach(incorrect_answer => console.log(incorrect_answer + '\n'));
+		console.log('\n');
 	}
 }
 
-obtenerDatos();
+function generarFormulario(){
+	const url="https://opentdb.com/api.php?amount=10";
+	obtenerDatos(url);
+}
