7d961ca51709e4d15abd873d88cc8c873fbdcefa
Mario Ruiz <mario.ruiz.ariza@gmail.com>
2023-12-17 20:30:48 +0100

    update

--- js/script.js
+++ js/script.js
@@ -12,8 +12,11 @@
 		//'Respuestas incorrectas: ' + datos.results[i].incorrect_answers + '\n'
 		//)
 	//}
-
 async function obtenerDatos(url){
+	const newDiv = document.createElement('div');
+	newDiv.classList.add('contenedor-preguntas');
+	document.querySelector(".contenedor").appendChild(newDiv);
+
 	const resultado = await fetch(url);
 	const datos = await resultado.json();
 	for(let i=0; i<datos.results.length; i++){
@@ -21,6 +24,21 @@ async function obtenerDatos(url){
 								datos.results[i].correct_answer+ '\n');
 		datos.results[i].incorrect_answers.forEach(incorrect_answer => console.log(incorrect_answer + '\n'));
 		console.log('\n');
+
+		const newH3 = document.createElement("h3")
+		newH3.textContent = `Pregunta ${i+1}. ${datos.results[i].question}`;
+		document.querySelector(".contenedor-preguntas").appendChild(newH3);
+		
+		const newP = document.createElement("p");
+		newP.classList.add('bold');
+		newP.textContent = datos.results[i].correct_answer;
+		document.querySelector(".contenedor-preguntas").appendChild(newP);
+
+		datos.results[i].incorrect_answers.forEach( incorrect_answer=> {
+			const newP = document.createElement("p");
+			newP.textContent = incorrect_answer; 
+			document.querySelector(".contenedor-preguntas").appendChild(newP);}
+		);
 	}
 }
 
