759cf740d71742babcc03a59e5bf4a8860cee4f7
Mario Ruiz <mario.ruiz.ariza@gmail.com>
2023-12-18 12:06:14 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,39 +1,56 @@
-async function obtenerDatos(url){
+function crearDiv(){
+	//Creo un nuevo div que contendrá los datos del fetch
 	const newDiv = document.createElement('div');
+	//Le asigno una clase css
 	newDiv.classList.add('contenedor-preguntas');
+	//Añado el elemento div al elemento con la clase ".contenedor"
 	document.querySelector(".contenedor").appendChild(newDiv);
+}
 
+function crearPregunta(indice, pregunta){
+	const newH3 = document.createElement("h3")
+	newH3.textContent = `Pregunta ${indice+1}. ${pregunta}`;
+	document.querySelector(".contenedor-preguntas").appendChild(newH3);
+}
+
+function crearRespuestaCorrecta(respuestaCorrecta){
+	const newPCorrect = document.createElement("p");
+	newPCorrect.classList.add('bold');
+	newPCorrect.textContent = `${respuestaCorrecta}`;
+	newPCorrect.addEventListener("click",function(){
+		newPCorrect.style.background = "lightgreen";
+		newPCorrect.style.color = "black";
+	})
+
+	document.querySelector(".contenedor-preguntas").appendChild(newPCorrect);
+}
+
+function crearRespuestasIncorrectas(respuestasIncorrectas){
+	respuestasIncorrectas.forEach(incorrect_answer=>{
+		const newP = document.createElement("p");
+		newP.textContent = incorrect_answer;
+		newP.addEventListener("click",function(){
+			newP.style.background="red";
+			newP.style.color="black";
+		})
+		document.querySelector(".contenedor-preguntas").appendChild(newP);
+	})
+}
+
+async function obtenerDatos(url){
 	const resultado = await fetch(url);
 	const datos = await resultado.json();
 
 	for(let i=0; i<datos.results.length; i++){
-		const newH3 = document.createElement("h3")
-		newH3.textContent = `Pregunta ${i+1}. ${datos.results[i].question}`;
-		document.querySelector(".contenedor-preguntas").appendChild(newH3);
-		
-		const newPCorrect = document.createElement("p");
-		newPCorrect.classList.add('bold');
-		newPCorrect.textContent = datos.results[i].correct_answer;
-		newPCorrect.addEventListener("click",function(){
-			newPCorrect.style.background = "lightgreen";
-			newPCorrect.style.color = "black";
-		})
-		document.querySelector(".contenedor-preguntas").appendChild(newPCorrect);
-
-		datos.results[i].incorrect_answers.forEach( incorrect_answer=> {
-			const newP = document.createElement("p");
-			newP.textContent = incorrect_answer;
-			newP.addEventListener("click",function(){
-				newP.style.background = "red";
-				newP.style.color = "black";
-			})
-			document.querySelector(".contenedor-preguntas").appendChild(newP);}
-		);
+		crearPregunta(i, datos.results[i].question);
+		crearRespuestaCorrecta(datos.results[i].correct_answer);
+		crearRespuestasIncorrectas(datos.results[i].incorrect_answers);
 	}
 }
 
 //Función que se llama cuando hago click al botón del formulario.
 function generarFormulario(){
 	const url="https://opentdb.com/api.php?amount=10";
+	crearDiv();
 	obtenerDatos(url);
 }
